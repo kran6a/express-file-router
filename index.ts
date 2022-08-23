08257bf537c5f738ee225aee224827474aa9ea21
kran6a <kran6a@gmail.com>
2022-08-23 21:26:22 +0200

    Initial commit

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import {Router} from "express";
-import {Application} from "express";
 import createRouter from "./router.js";
 
 export default createRouter;
 
 export { createRouter, Options };
-export const router: (options?: Options) => Promise<Application> = (options: Options = {}) => createRouter(Router(), options);
+export const router: (options?: Options) => Promise<Router> = (options: Options = {}) => createRouter(Router(), options);
