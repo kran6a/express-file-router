25769f1c8bca7b40cd1a6af182b59cf1b4a99aa9
kran6a <kran6a@gmail.com>
2022-08-24 22:37:29 +0200

    Initial commit

--- global.d.ts
+++ global.d.ts
@@ -22,7 +22,7 @@ export type Route = {
         [x: string]: Endpoint
     }
 }
-export type Endpoint = <T>({params, request, url, middleware}: { params: Partial<{ [x: string]: string }>, request: Request, url: URL, middleware: Record<string, T> }) => Endpoint_Response | Promise<Endpoint_Response>
+export type Endpoint = <T extends Record<string, any>>({params, request, url, middleware}: { params: Partial<{ [x: string]: string }>, request: Request, url: URL, middleware: Record<string, T> }) => Endpoint_Response | Promise<Endpoint_Response>
 export type Endpoint_Response = {
     status: number,
     headers?: Record<string, string | number>,
